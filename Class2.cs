010adddab6592b8ddd6644f0d5487e9b2abb4b90
chris <chris@chris-PC>
2017-04-03 11:57:31 -0700

    fixed conflict

--- Class2.cs
+++ Class2.cs
@@ -7,8 +7,15 @@ public class Class1
 {
 	public Class1()
 	{
+
+        int len = 5;
+
         Console.WriteLine("hello earth");
 
-        
+        for (int i = 1; i < len; i++)
+        {
+            Console.WriteLine("*");
+        }
+
 	}
 }
