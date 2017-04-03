00c90462228e77c47ce8620964eac1a27f546d1d
HudaSyed <HudaSyed@STU-F-046.student.cascadia.edu>
2017-04-03 11:44:10 -0700

    added loop in class

--- Class2.cs
+++ Class2.cs
@@ -7,6 +7,13 @@ public class Class1
 {
 	public Class1()
 	{
+        int len = 5;
+
         Console.WriteLine("hello world");
+
+        for (int i = 1; i < len; i++)
+        {
+            Console.WriteLine("*");
+        }
 	}
 }
